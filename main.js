f9b497b05e92f9e883a47da79a00ee9961860171
Duyvip- 14 <duylun070502@gmail.com>
2024-11-07 18:52:23 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+/** commnet trong js 
+ * 
+*/
+var a= 2
+var c = a++ + --a;
+// 2 + 2 = 4 vì a++ lúc này vẫn là 2 
+// nhưng sau khi xong a++ sẽ là 3 
+// rồi gặp a-- sẽ thành 2 nên ta có là 2+2 =4
+var a = 'duy hêhe'
+var b = 'duy hêhe'
+if ( a ==b) {
+    console.log(' đúng mẹ nó rồi1!')
+    
+} else {
+    console.log( 'Sai!!!')
+    
+}
+
+
+console.log('c',c)
+ 
+
+
+
